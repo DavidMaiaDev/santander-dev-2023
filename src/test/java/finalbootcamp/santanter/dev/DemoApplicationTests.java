dfaa3a84ddd817fa0f25b03575a54f3bccb17e98
David Gonçalves Maia <david.goncalves@dcx.ufpb.br>
2023-10-13 20:02:17 -0300

    Initial commit

---
+++ src/test/java/finalbootcamp/santanter/dev/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package finalbootcamp.santanter.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
